ebadd71e03ab3c18d9394acec23c789be7d11159
Wadson Garbes <nenogarbes@hotmail.com>
2023-12-05 15:44:43 -0300

    v1.0

---
+++ popup.js
@@ -0,0 +1,33 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+  const hardcodedUrlsWithLabels = [
+    { label: 'Gestão PROD', url: 'https://gestao.pontotel.com.br' },
+    { label: 'Bateponto PROD', url: 'https://bateponto.pontotel.com.br' },
+    { label: 'Intranet PROD', url: 'https://intranet.pontotel.com.br' },
+    { label: 'Papo', url: 'https://papo.pontotel.com.br' },
+    { label: 'Gestão HML', url: 'https://hml.pontotel.com.br/pontotel/new-front/index.html#/cognito/login' },
+    { label: 'Intranet HML', url: 'https://hml.pontotel.com.br/pontotel/back' }
+  ];
+
+  const urlsList = document.getElementById('urlsList');
+
+  // Display hardcoded labels with buttons
+  hardcodedUrlsWithLabels.forEach(function (entry) {
+    const buttonItem = document.createElement('button');
+    buttonItem.textContent = entry.label;
+
+    // Store the URL as a data attribute
+    buttonItem.dataset.url = entry.url;
+
+    // Add a click event listener to open the link
+    buttonItem.addEventListener('click', function () {
+      const url = buttonItem.dataset.url;
+      if (url) {
+        // Open the link in a new tab
+        chrome.tabs.create({ url: url });
+      }
+    });
+
+    urlsList.appendChild(buttonItem);
+  });
+});
