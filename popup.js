a8975a667d01a18859a38d1208ef78ded3223e30
Wadson Garbes <nenogarbes@hotmail.com>
2024-10-17 10:19:18 -0300

    Update popup.js to v1.3
    Remove old logic and display everything on popup instead

--- popup.js
+++ popup.js
@@ -1,34 +1,7 @@
-
-document.addEventListener('DOMContentLoaded', function () {
-  const hardcodedUrlsWithLabels = [
-    { label: 'Gestão PROD', url: 'https://gestao.pontotel.com.br' },
-    { label: 'Bateponto PROD', url: 'https://bateponto.pontotel.com.br' },
-    { label: 'Intranet PROD', url: 'https://intranet.pontotel.com.br' },
-    { label: 'Aloque PROD', url: 'https://aloque.pontotel.com.br/' },
-    { label: 'Aloque HML', url: 'https://tinyurl.com/4c3339sy' },
-    { label: 'Gestão HML', url: 'https://hml.pontotel.com.br/pontotel/new-front/index.html#/cognito/login' },
-    { label: 'Intranet HML', url: 'https://hml.pontotel.com.br/pontotel/back' },
-  ];
-
-  const urlsList = document.getElementById('urlsList');
-
-  // Display hardcoded labels with buttons
-  hardcodedUrlsWithLabels.forEach(function (entry) {
-    const buttonItem = document.createElement('button');
-    buttonItem.textContent = entry.label;
-
-    // Store the URL as a data attribute
-    buttonItem.dataset.url = entry.url;
-
-    // Add a click event listener to open the link
-    buttonItem.addEventListener('click', function () {
-      const url = buttonItem.dataset.url;
-      if (url) {
-        // Open the link in a new tab
-        chrome.tabs.create({ url: url });
-      }
+document.querySelectorAll('.btn').forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent the default anchor behavior
+        const url = link.getAttribute('href'); // Get the URL from href
+        chrome.tabs.create({ url: url }); // Open in a new tab
     });
-
-    urlsList.appendChild(buttonItem);
-  });
 });
